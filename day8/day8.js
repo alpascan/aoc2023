2338154d848447978411b29966d90142fe45fbe7
Alexandru Pascan <alpascan@gmail.com>
2023-12-16 12:13:18 +0100

    Added day8 p1

--- day8/day8.js
+++ day8/day8.js
@@ -8,19 +8,35 @@ function readInput(file_name) {
   return input.split('\n');
 }
 
-input = readInput("ex1.txt");
+input = readInput("ex2.txt");
 road = input[0]
 
 input.splice(0, 2).filter(x => x)
 a = new Object()
 for (let i = 0; i < input.length; i++) {
   f = input[i].replace('(', '').replace(')', '').split(' = ')
+  if (f == '') {
+    continue
+  }
   ii = f[1].split(', ')
-  console.log(f[0], ii)
-  a[f[0]] = { left: ii[0], right: ii[1] }
+  a[f[0]] = { L: ii[0], R: ii[1] }
 
 }
 console.log(road)
 console.log(a)
 
 
+let currentNode = 'AAA'
+let steps = 0
+let directions = road.split('');
+console.log(directions)
+while (currentNode != 'ZZZ') {
+  for (let i in directions) {
+
+    console.log(currentNode + ' ' + i + ' ' + directions[i])
+    currentNode = a[currentNode][directions[i]]
+    steps += 1
+  }
+}
+
+console.log(steps)
